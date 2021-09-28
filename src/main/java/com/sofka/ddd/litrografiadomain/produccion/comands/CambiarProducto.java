770ab1f96d05e211c0271b767d71a8d6e4176ba4
Leonardo <leocv0205@gmail.com>
2021-09-28 02:29:53 -0500

    Se ecrean el ultimo comando del agregado produccion

--- src/main/java/com/sofka/ddd/litrografiadomain/produccion/comands/CambiarProducto.java
+++ src/main/java/com/sofka/ddd/litrografiadomain/produccion/comands/CambiarProducto.java
@@ -1,6 +1,24 @@
 package com.sofka.ddd.litrografiadomain.produccion.comands;
 
 import co.com.sofka.domain.generic.Command;
+import com.sofka.ddd.litrografiadomain.produccion.values.IdProduccion;
+import com.sofka.ddd.litrografiadomain.produccion.values.Producto;
 
 public class CambiarProducto extends Command {
+    private  final IdProduccion idProduccion;
+    private final Producto producto;
+
+    public CambiarProducto(IdProduccion idProduccion, Producto producto) {
+        this.idProduccion = idProduccion;
+        this.producto = producto;
+    }
+
+    public IdProduccion getIdProduccion() {
+        return idProduccion;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
 }
+
